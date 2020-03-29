aa06bdcc72d1e25190c788e9749a01d7a8a6ef6a
Gabriel Baker <me@g403.co>
2020-03-29 13:41:08 +0100

    Remove final findViewById

--- app/src/main/java/co/g403/android/viewpager2demo/ui/home/HomeFragment.kt
+++ app/src/main/java/co/g403/android/viewpager2demo/ui/home/HomeFragment.kt
@@ -6,29 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.viewpager2.widget.ViewPager2
 import co.g403.android.viewpager2demo.R
 import co.g403.android.viewpager2demo.testimonials.TestimonialAdapter
-
+import kotlinx.android.synthetic.main.testimonials_layout.view.*
 
 class HomeFragment : Fragment() {
 	private val viewModel: HomeViewModel = HomeViewModel()
 	private val testimonialsAdapter: TestimonialAdapter = TestimonialAdapter()
-	private lateinit var viewPager: ViewPager2
-
 
 	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
+		inflater: LayoutInflater,
+		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		val view = inflater.inflate(
-			R.layout.home_fragment, container,
-			false
-		)
-		viewPager = view.findViewById(R.id.testimonials_view_pager)
-		viewPager.adapter = testimonialsAdapter
+		val view = inflater.inflate(R.layout.home_fragment, container, false)
+		view.testimonials_view_pager.adapter = testimonialsAdapter
 		return view
-
 	}
 
 	override fun onActivityCreated(savedInstanceState: Bundle?) {
